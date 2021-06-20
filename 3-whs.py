46a306939ab15358766bd5e04c8a9141fac6e161
RelaxDude <64224648+YonatanMualem@users.noreply.github.com>
2021-06-20 12:59:44 +0300

    Update 3-whs.py

--- 3-whs.py
+++ 3-whs.py
@@ -12,6 +12,7 @@ AcceptQueue = Queue(1000)
 OpenConnection = []
 PORT = 8000
 
+
 def Port_RST_Drop():
     '''
     Drop kernel auto RST responses to packets that come into a specific port
@@ -20,35 +21,35 @@ def Port_RST_Drop():
     os.system("sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST --sport 80 -j DROP")
 
 
-
 class PacketsOrginize(Thread):
     '''
      Listen to all packets that coming through a specific port.
     '''
+
     def run(self):
-        #Sniff tcp packets with specific port
+        # Sniff tcp packets with specific port
         sniff(filter="tcp and dst port " + str(PORT), prn=self.PacketTransfer, store=0)
 
     def PacketTransfer(self, pkt):
-        connction = {"IP":pkt["IP"].dst, "SPORT": pkt["TCP"].SPORT}
-        #Check if the user already have a connection to the server
+        connction = {"IP": pkt["IP"].dst, "SPORT": pkt["TCP"].SPORT}
+        # Check if the user already have a connection to the server
         if IP in pkt and connction in AcceptQueue.queue:
-            #Send packet to start get data
+            # Send packet to start get data
             AcceptQueue.put(pkt)
         else:
-            #Send packet to create 3 way handshake
+            # Send packet to create 3 way handshake
             PacketFilter.put(pkt)
-
 
 
 class PacketSplitter(Thread):
     def __init__(self):
         super().__init__()
         # Queue for handle all wait for ack
-		# [{"pkt:packet, time: time.time()},]
+        # [{"pkt:packet, time: time.time()},]
         self.SynList = []
         # Queue for handle all ACK packets
         self.AckQueue = Queue(1000)
+        Thread(target= self.SynQueue()).start()
 
     def SYNACK_create(self, packet):
         '''
@@ -63,10 +64,12 @@ class PacketSplitter(Thread):
         AckNr = packet["TCP"].seq + 1
         # Create syn-ack packet with scapy
         synack = IP(dst=saddr) / TCP(sport=dport, dport=sport, flags="SA", seq=SeqNr, ack=AckNr)
-		#Need ro send synack
-		
-		ACK = IP(dst= saddr) / TCP(sport=dport, dport=sport, flags = "A" , ack = SeqNr+1)
-        return {"pkt":ACK, "time": datetime.time}
+        # Need ro send synack
+
+        ACK = IP(dst=saddr) / TCP(sport=dport, dport=sport, flags="A", ack=SeqNr + 1)
+
+        return {"pkt": ACK, "time": datetime.time}
+
 
     def run(self):
         '''
@@ -80,7 +83,7 @@ class PacketSplitter(Thread):
                 SYN = 0x02
                 ACK = 0x10
                 # Check if packet flag is SYN and the queue is full
-                if Flag & SYN and len(self.SynList) < MaxHalfConnection :
+                if Flag & SYN and len(self.SynList) < MaxHalfConnection:
                     packet = self.SYNACK_create(packet)
                     # Add packet to SynQueue (need to change description)
                     self.SynList.append(packet)
@@ -100,24 +103,32 @@ class PacketSplitter(Thread):
         '''
         while True:
             # Check if AckQueue is empty
-			if not self.AckQueue.empty():
-				ACK_Packet = self.AckQueue.get()
-				count =0
-				for packet in self.SynList:
-					# Check if ACK packet is match one in the SynQueue
-					if packet["pkt"] == ACK_Packet:
-						IP = packet["IP"].src
-						SPORT = packet["TCP"].sport
-						CompleteConnection = {"IP": IP, "SPORT": SPORT}
-						# Add packet to Established connections
-						AcceptQueue.put(CompleteConnection)
-						print("Connection established!! \n ip:", IP + "\n port:", SPORT)
-						break
-					else: 
-					if datetime.time - packet["time"] >75:
-						self.SynList.remove[packet]
-					else: 
-						count +=1
-				if count>700:
-					print("Syn flood attack detect")
-					self.SynList.clear()
+            if not self.AckQueue.empty():
+                ACK_Packet = self.AckQueue.get()
+                count = 0
+                for packet in self.SynList:
+                    # Check if ACK packet is match one in the SynQueue
+                    if packet["pkt"] == ACK_Packet:
+                        IP = packet["IP"].src
+                        SPORT = packet["TCP"].sport
+                        CompleteConnection = {"IP": IP, "SPORT": SPORT}
+                        # Add packet to Established connections
+                        AcceptQueue.put(CompleteConnection)
+                        print("Connection established!! \n ip:", IP + "\n port:", SPORT)
+                        break
+                    else:
+                        if datetime.time - packet["time"] > 75:
+                            self.SynList.remove[packet]
+                        else:
+                            count += 1
+                if count > 700:
+                    print("Syn flood attack detect")
+                    self.SynList.clear()
+
+
+def main():
+    Port_RST_Drop()
+    server = PacketsOrginize()
+    server1 = PacketSplitter().__init__()
+    Thread(target=server).start()
+    Thread(target=server1).start()
